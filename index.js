9f16b6f73bad9d1e809b0efea3b4e9fecf98e96a
Ivan Khomenko <simkav2411@gmail.com>
2021-07-14 22:57:16 +0300

    add morgan

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const helmet = require('helmet')
 const compression = require('compression')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 app.use(helmet())
 app.use(compression())
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/', (req, res, next) => {
   res.status(200).end('Bye')
