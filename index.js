dc7431979c50da5b36ba608134cfbfcf5946ab55
Ivan Khomenko <simkav2411@gmail.com>
2021-07-15 10:17:33 +0300

    add static example

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(compression())
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static('./static', { extensions: ['html'] }))
 
 app.get('/', (req, res, next) => {
   res.status(200).end('Bye')
